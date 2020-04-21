8ad639178b29d2cb31a2dcc6588310af8fdff283
qwx <2580721685@qq.com>
2020-04-21 15:25:42 +0800

    1 Add floor display function 2 The function is realized by playing GIF

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,5 +1,7 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QLabel>
+#include <QMovie>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -9,6 +11,13 @@ MainWindow::MainWindow(QWidget *parent) :
 
     //移除边框
     setWindowFlags(Qt::FramelessWindowHint);
+
+
+    /*显示楼层功能*/
+    QMovie *movie_number = new QMovie(":/gif/number.gif");  //播放资源文件 gif 路径的number.gif
+    ui->number->setMovie(movie_number);                     //指定数据显示在number控件上面
+    movie_number->start();                                  //开始播放
+
 }
 
 MainWindow::~MainWindow()
